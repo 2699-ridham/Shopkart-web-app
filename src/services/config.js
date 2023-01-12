9bba62c34efc293c2cd8560bf66f49b0b4bff107
ridham.2699 <ridhamgarg786@gmail.com>
2023-01-12 20:14:43 +0530

    first commit

---
+++ src/services/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyANmcIQR9VtPEnNYh2nhZV7Hu5-XFam9n0",
+  authDomain: "shopkart-3cd88.firebaseapp.com",
+  projectId: "shopkart-3cd88",
+  storageBucket: "shopkart-3cd88.appspot.com",
+  messagingSenderId: "745442068983",
+  appId: "1:745442068983:web:de6be99f6f8dad2f0446f2",
+  measurementId: "G-4ENHYWPQ5F"
+};
+
+export default firebaseConfig;
